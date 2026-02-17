1f612d934c051d110748c1bd8580b84df0f6f507
yakovfumm@gmail.com <you@example.com>
2026-02-17 13:56:25 +0200

    implemented Miller-Rabin primality test

--- RSA.py
+++ RSA.py
@@ -1,5 +1,6 @@
+import random
 import mod
-
+import time
 
 def RSA_init():
     nums = mod.find_primes(2)
@@ -8,32 +9,55 @@ def RSA_init():
     e = mod.find_e(phin)
     return (nums, n, phin, e)
 
-data = RSA_init()
+def RSA_encrypt(m: int, n: int, e: int):
+    return pow(m,e,n)
 
-def RSA_encrypt(m: int):
-    global data
-    return pow(m,data[3],data[1])
+def RSA_decrypt_naive(c: int, n: int, e: int, phin: int):
+    d = mod.mod_inverse(e,phin)
+    return pow(c, d, n)
 
-def RSA_decrypt_naive(c: int):
-    global data
-    d = mod.mod_inverse(data[3],data[2])
-    return pow(c, d, data[1])
-
-def RSA_decrypt_CRT(c: int):
-    global data
-    d = mod.mod_inverse(data[3],data[2])
-    yq = mod.mod_inverse(data[0][0], data[0][1])
-    yp = mod.mod_inverse(data[0][1], data[0][0])
-    c_p = c%data[0][0]
-    c_q = c%data[0][1]
+def RSA_decrypt_CRT(c: int, e: int, n: int, phin: int, nums: list[int]):
+    d = mod.mod_inverse(e,phin)
+    yq = mod.mod_inverse(nums[0], nums[1])
+    yp = mod.mod_inverse(nums[1], nums[0])
+    c_p = c%nums[0]
+    c_q = c%nums[1]
 
     #Fermat's little theorem
-    d_p = d % data[0][0]
-    d_q = d % data[0][1]
+    d_p = d % nums[0]
+    d_q = d % nums[1]
 
     #solve system
-    m_p = pow(c_p,d_p,data[0][0])
-    m_q = pow(c_q,d_q,data[0][1])
+    m_p = pow(c_p,d_p,nums[0])
+    m_q = pow(c_q,d_q,nums[1])
 
     #CRT part
-    return (m_p*data[0][0]*yp + m_q*data[0][1]*yq) % data[1]
+    return (m_p*nums[0]*yp + m_q*nums[1]*yq) % n
+
+def main():
+    print("beginning RSA...")
+    data = RSA_init()
+    private_key = [data[0], data[2]]
+    public_key = [data[1],data[3]]
+
+    message = random.randint(1,1000000)
+    print("encrypting: ", message)
+    encrypted = RSA_encrypt(message, public_key[0], public_key[1])
+    print("encrypted message: ", encrypted)
+
+    start_time = time.perf_counter()
+
+    print("decryption with naive RSA..")
+    print("Decrypted: ", RSA_decrypt_naive(encrypted, public_key[0], public_key[1], private_key[1]))
+    end_time = time.perf_counter()
+    print("total time: ", end_time-start_time)
+
+    start_time = time.perf_counter()
+
+    print("decryption with CRT RSA..")
+    print(RSA_decrypt_CRT(encrypted, public_key[0], public_key[1], private_key[1], private_key[0]))
+    end_time = time.perf_counter()
+    print("total time: ", end_time-start_time)
+
+if __name__ == "__main__":
+    main()
--- mod.py
+++ mod.py
@@ -1,6 +1,6 @@
 import random
-min_range = pow(2, 100)
-max_range = pow(2, 101) - 1
+min_range = pow(2, 1024)
+max_range = pow(2, 1025) - 1
 #Uses naive algorithm to check if num is prime
 def possible_prime(num: int, bound: int) -> bool:
     for i in range(2,bound):
@@ -10,28 +10,44 @@ def possible_prime(num: int, bound: int) -> bool:
             return False
     return True
 
-def miller_rabin(num: int, iterations: int) -> int:
-    
+#Uses miller-rabin primality test; page 156 rosen; to test if num is prime in a random base a for some iterations
+#after the iterations, has 0.25^iterations chance to be composite
+def miller_rabin(num: int, iterations: int) -> bool:
+    temp = num - 1
+    #find s,t such that num - 1 = d*(2^s)
+    s = 0
+    while(temp%2 == 0):
+        temp //= 2
+        s += 1
+    d = temp
 
-    return 0
+    #check iterations times if num passes miller's test for base a
+    for i in range(0, iterations):
+        a = random.randint(2,num-2)
+        x = pow(a,d,num)
+        for j in range(0,s):
+            x = pow(x,2,num)
+        if x != 1:
+            return False
+    return True
+
+#Returns a list[amount] with higly likely primes
 def find_primes(amount: int) -> list[int]:
     global min_range,max_range
-    found = 0
     res = []
-    candidate = random.randint(min_range,max_range)
-    if(candidate % 2 == 0):
-        candidate += 1
-    while found != amount:
-        print(candidate)
+    while amount != 0:
+        #get random odd int in range
+        candidate = random.randint(min_range,max_range)
+        if(candidate % 2 == 0):
+            candidate += 1
         if possible_prime(candidate, 10000):
-            print("found!!")
-            res.append(candidate)
-            found += 1
-        candidate += 2
+            if miller_rabin(candidate, 100):#so (0.25)^100 chance to not be prime
+                res.append(candidate)
+                amount -= 1
     return res 
 
-#calculate extended GCD and also x,y with euclids alg such that ax+by=gcd(a,b)
-def extended_gcd(a: int,b: int) -> tuple:
+#calculate extended GCD and also x,y with euclids alg such that ax+by=gcd(a,b) recuresivly
+def extended_gcd(a: int,b: int) -> tuple[int,int,int]:
     if b == 0:
         return a , 0, 1
     gcd, x, y = extended_gcd(b,a%b)
